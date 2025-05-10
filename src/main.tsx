2cf6e527d3724bcaa21cbff3078f2bfa2765e6aa
Andriy Romanov <mgm.agro04@gmail.com>
2025-05-10 13:26:34 +0300

    add normalize and global style

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+// Нормалізація стилів add
+import "modern-normalize";
+// Глобальні стилі (додатково) add
+import "./global.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
